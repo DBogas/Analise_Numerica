343de64890d4333d05614b9f6106beb9901f4f0d
jCampFerr <joao.mcf@hotmail.com>
2017-02-18 23:43:12 +0000

    AN_trab1_ex3 - Ultima versão

--- trab1/AN_trab1_ex3.java
+++ trab1/AN_trab1_ex3.java
@@ -3,36 +3,32 @@ public class AN_trab1_ex3 {
 
 	public static void main(String[] args){
 		
-		int inf = 92; //para 93 ja nao funciona (INF/INF)
-		double pi=0, num, denum, primeiro_exp, primeiro_elemento, segundo_elemento, fact=1;
+		int inf = 200; 
+		double pi=2, num, res=2, denum,  num1, denum1, denum2, checkError=0.00001, error;
+		int conta=5;
+		boolean check=false;
 		
-		for(int i=1; i<inf; i++){
-			primeiro_exp = (double)i+1;
-			primeiro_elemento = Math.pow((double)2, (double)primeiro_exp);
-			fact*=i;
-			segundo_elemento = Math.pow((double)fact, (double)2);
-			num = (double)primeiro_elemento*segundo_elemento;
-			denum = (double)factorial(2*i+1);
-			//System.out.println("1elemt: "+primeiro_elemento+" 2elemt: "+segundo_elemento+" NUM:"+num+" DENUM:"+denum);
-			pi+=(double)num/denum;
-			//System.out.println(i+ " " +pi);
+		for(int i=0; i<inf; i++){
+			num1 = Math.pow((double)(i+1), (double)2);
+			num = num1 * (double)2;
+			denum1 = (double)(2*i)+(double)3;
+			denum2 = (double)(2*i)+(double)2;
+			denum = denum1*denum2;
+			res=res*(num/denum);
+			pi+=res;
+			error = Math.PI-pi;
+			if(error<checkError && !check){
+				System.out.println("Para um erro absoluto de 10^-"+conta+":");
+				System.out.println("O numero n de termos usados na série: " + i);
+				System.out.println("Sn, o valor aproximado de Pi: " + pi);
+				System.out.println("O erro absoluto efetivamente cometido no cálculo de Pi, En = |Pi-Sn|: " + error);
+				System.out.println();
+				conta++;
+				checkError/=10;
+				if(conta==13)
+					check=true;
+			}
+			//System.out.println("I:"+i+" PI:"+pi+" ERRO:"+error);
 		}
-		
-		double error = Math.PI-pi;
-		
-		System.out.println("O numero n de termos usados na serie: " + inf);
-		System.out.println("Sn, o valor aproximado de Pi: " + pi);
-		System.out.println("O erro absoluto efetivamente cometido no calculo de Pi, En = |Pi-Sn|: " + error);
-		
 	}
-	
-	public static double factorial(int n){
-			
-		double result=n;
-		for(int i=n-1; i>0; i--)
-			result*=i;
-			
-		return result;
-	}
-	
 }
