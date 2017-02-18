b33608d5aba71613ed342d51209f475b12d04e66
jCampFerr <joao.mcf@hotmail.com>
2017-02-18 18:32:48 +0000

    AN_trab1_ex1

--- trab1/AN_trab1_ex1.java
+++ trab1/AN_trab1_ex1.java
@@ -3,20 +3,12 @@ public class AN_trab1_ex1 {
 
 	public static void main(String[] args){
 		
-		int N=100000; //para N=100000    => 1.1102230246251568E-16
-					  //para N=999999999 => 1.1102230246251568E-16
+		double eps = 1;
 		
-		double max=1.0, min=0.0, eps;
-	   
-		for(int i=0; i<N; i++){
-			eps = (max+min)/((double)2.0);
-			double temp = ((double)1.0)+eps;
-			if(temp==((double)1.0))
-				min=eps;
-			else
-				max=eps;
-		}
+		while(eps+1>1)
+			eps/=2;
 		
-		System.out.println("O epsilon maquina e: " + max);
+		System.out.println("O Epsilon máquina (1) é: " + eps);
+		System.out.println("O Epsilon máquina (2) é: " + eps*2);
 	}
 }
