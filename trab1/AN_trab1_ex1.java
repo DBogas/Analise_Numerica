712d9547ed5204bc1d4b8c74c1001ae2fad1de34
dmrfer <up201403232@fc.up.pt>
2017-02-19 17:20:04 +0000

    Update AN_trab1_ex1.java
    Adicionado um contador de iterações para ficar mais "completo" e para ser mais fácil de explicar
    Adicionado código para obter epsilon em precisao simples (apenas por curiosidade)

--- trab1/AN_trab1_ex1.java
+++ trab1/AN_trab1_ex1.java
@@ -10,5 +10,28 @@ public class AN_trab1_ex1 {
 		
 		System.out.println("O Epsilon máquina (1) é: " + eps);
 		System.out.println("O Epsilon máquina (2) é: " + eps*2);
+		
+		/*
+		{
+			System.out.println("*Precisao simples*");
+			float eps = 1f;
+			int it=0;
+			while(eps+1f>1f){
+				eps/=2f;
+				it++;
+			}
+			System.out.println("("+(it-1)+" iteraçoes): O Epsilon máquina é: " + eps*2);
+		}
+		
+		{
+			System.out.println("*Precisao dupla*");
+			double eps = 1;
+			int it=0;
+			while(eps+1>1){
+				eps/=2;
+				it++;
+			}
+			System.out.println("("+(it-1)+"iteraçoes): O Epsilon máquina é: " + eps*2);
+		}*/
 	}
 }
