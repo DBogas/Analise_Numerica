572022723ebd9f3a04b2af428cd60578b599fbcb
IuriTPena <iuritavarespena@gmail.com>
2017-05-14 17:54:37 +0100

    code cleaning

--- trab4/trap.java
+++ trab4/trap.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 class trap {
     public static void main(String[] args) {
         
-        Scanner in = new Scanner(System.in);
         System.out.println("Calcular o integral de sin(sin(sin(sin(x)))), entre 0 e 2:");
         System.out.println();
 
         int a = 0;
         int b = 2;
 
-        double t = (b-a)/2;
+        double i = (b-a);
+        double t = i/2;
         double e = 5*Math.pow(10, -8);
         double m = 1.088549352507347;
         
-        int n = definirN(a, b, e, m);
-        System.out.println("N: "+n);
-        double h = (b-a)/n;
+        int n = definirN(i, e, m);
+        double h = i/n;
+
+        System.out.println("N = "+n+"\tH = "+h);
     }
 
-    static int definirN(int a, int b, double e, double m) {
+    static int definirN(double i, double e, double m) {
         int n = 1;
         while(true) {
-            double h = (double)(b-a)/n;
-            double err = -1 * (Math.pow(h,2)/12) * (b-a) * m;
-            if(n == 512)
-                System.out.println("H = "+h+"\nErro =  "+err);
+            double h = i/n;
+            double err = -1 * (Math.pow(h,2)/12) * i * m;
             if(Math.abs(err) < e)
                 return n;
             n++;
